Fixed accuracy calculation

scripts/engine.py
@@ -39,42 +39,44 @@ def train_step(model: torch.nn.Module,
         (0.1112, 0.8743)
     """
     
-    # Put model in training mode
+    # Put model in train mode
     model.train()
-    
+
     # Setup train loss and train accuracy values
     train_loss, train_acc = 0, 0
-    
+
     # Loop through data loader data batches
     for batch, (X, y) in enumerate(dataloader):
         # Send data to target device
         X, y = X.to(device), y.to(device)
-        
+
         # 1. Forward pass
         y_pred = model(X)
-        
-        # 2. Calculate and accumulate loss
+
+        # 2. Calculate  and accumulate loss
         loss = loss_fn(y_pred, y)
-        train_loss += loss.item()
-        
+        train_loss += loss.item() 
+
         # 3. Optimizer zero grad
         optimizer.zero_grad()
-        
+
         # 4. Loss backward
         loss.backward()
-        
+
         # 5. Optimizer step
         optimizer.step()
-        
+
         # Calculate and accumulate accuracy metric across all batches
         y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
-        train_acc += (y_pred_class == y).sum().item() / len(y_pred)
-        
-    # Adjust metrics to get average loss and accuracy per batch
+        train_acc += (y_pred_class == y).sum().item()/len(y_pred)
+
+    # Adjust metrics to get average loss and accuracy per batch 
     train_loss = train_loss / len(dataloader)
     train_acc = train_acc / len(dataloader)
     
-    return train_loss, train_loss
+    return train_loss, train_acc
+
+
 
 # Test step function
 def test_step(model: torch.nn.Module, 
@@ -103,34 +105,36 @@ def test_step(model: torch.nn.Module,
     """
     
     # Put model in eval mode
-    model.eval()
-    
+    model.eval() 
+
     # Setup test loss and test accuracy values
     test_loss, test_acc = 0, 0
-    
+
     # Turn on inference context manager
     with torch.inference_mode():
         # Loop through DataLoader batches
         for batch, (X, y) in enumerate(dataloader):
             # Send data to target device
             X, y = X.to(device), y.to(device)
-            
+
             # 1. Forward pass
             test_pred_logits = model(X)
-            
+
             # 2. Calculate and accumulate loss
             loss = loss_fn(test_pred_logits, y)
             test_loss += loss.item()
-            
+
             # Calculate and accumulate accuracy
             test_pred_labels = test_pred_logits.argmax(dim=1)
-            test_acc += ((test_pred_labels == y).sum().item() / len(test_pred_labels))
-    
-    # Adjust metrics to get average loss and accuracy per batch
+            test_acc += ((test_pred_labels == y).sum().item()/len(test_pred_labels))
+
+    # Adjust metrics to get average loss and accuracy per batch 
     test_loss = test_loss / len(dataloader)
     test_acc = test_acc / len(dataloader)
     
     return test_loss, test_acc
+
+
 
 # Train function
 # Updated: to include writer, so that it can be used with TensorBoard
@@ -140,8 +144,9 @@ def train(model: torch.nn.Module,
           optimizer: torch.optim.Optimizer,
           loss_fn: torch.nn.Module,
           epochs: int,
-          device: torch.device,
-          writer:SummaryWriter=None) -> Dict[str, List]:
+          device: torch.device, 
+          writer: SummaryWriter=None # new parameter to take in a writer
+          ) -> Dict[str, List]:
     """
     Trains and test a PyTorch model.
     
@@ -184,46 +189,37 @@ def train(model: torch.nn.Module,
     results = {"train_loss": [],
                "train_acc": [],
                "test_loss": [],
-               "test_acc": [],
-               "epoch_duration": []
+               "test_acc": []
     }
-    
+
     # Loop through training and testing steps for a number of epochs
     for epoch in tqdm(range(epochs)):
-        start_time = time.time()
-        
-        # Train metrics
         train_loss, train_acc = train_step(model=model,
                                            dataloader=train_dataloader,
                                            loss_fn=loss_fn,
                                            optimizer=optimizer,
                                            device=device)
         
-        # Test metrics
         test_loss, test_acc = test_step(model=model,
                                         dataloader=test_dataloader,
                                         loss_fn=loss_fn,
                                         device=device)
-        
-        end_time = time.time()
-        epoch_duration = end_time - start_time
-        
-        # Print output
+
+        # Print out what's happening
         print(
-            f"Epoch: {epoch+1} | "
-            f"train_loss: {train_loss:.4f} | "
-            f"train_acc: {train_acc:.4f} | "
-            f"test_loss: {test_loss:.4f} | "
-            f"test_acc: {test_acc:.4f}"
+          f"Epoch: {epoch+1} | "
+          f"train_loss: {train_loss:.4f} | "
+          f"train_acc: {train_acc:.4f} | "
+          f"test_loss: {test_loss:.4f} | "
+          f"test_acc: {test_acc:.4f}"
         )
-        
+
         # Update results dictionary
         results["train_loss"].append(train_loss)
         results["train_acc"].append(train_acc)
         results["test_loss"].append(test_loss)
         results["test_acc"].append(test_acc)
-        results["epoch_duration"].append(epoch_duration)
-        
+
         # Use the writer parameter to track experiments
         if writer: # See if there's a writer, if so, log to it
             # Add results to SummaryWriter
@@ -235,12 +231,11 @@ def train(model: torch.nn.Module,
                                tag_scalar_dict={"train_acc": train_acc,
                                                 "test_acc": test_acc}, 
                                global_step=epoch)
-            writer.add_scalar("Epoch Duration", epoch_duration, epoch)
 
             # Close the writer
             writer.close()
         else:
             pass
-    
+
     # Return the filled results at the end of the epochs
     return results