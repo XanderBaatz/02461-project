2bf096f86bb735e533b8ae26f965f0b9e20a0de0
Xander Baatz <lorentzen.alexander@gmail.com>
2024-01-11 08:56:27 +0100

    removed custom model again

--- scripts/model.py
+++ scripts/model.py
@@ -64,43 +64,6 @@ class TinyVGG(nn.Module):
         # Using operator fusion (faster)
         return self.classifier(self.conv_block_2(self.conv_block_1(x)))
 
-class TinyVGG_custom(nn.Module):
-    def __init__(self,
-                 input_shape: int,
-                 hidden_units: int,
-                 output_shape: int) -> None:
-        super(TinyVGG, self).__init__()
-
-        self.conv_block_1 = nn.Sequential(
-            nn.Conv2d(input_shape, hidden_units, kernel_size=3, stride=1, padding=0),
-            nn.ReLU(),
-            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, stride=1, padding=0),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-
-        self.conv_block_2 = nn.Sequential(
-            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=0),
-            nn.ReLU(),
-            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=0),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-
-        # Calculate the flattened size dynamically based on the input size
-        x = torch.randn(1, input_shape, 64, 64)
-        x = self.conv_block_1(x)
-        x = self.conv_block_2(x)
-        flattened_size = x.view(1, -1).shape[1]
-
-        self.classifier = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(in_features=flattened_size, out_features=output_shape)
-        )
-
-    def forward(self, x: torch.Tensor):
-        return self.classifier(self.conv_block_2(self.conv_block_1(x)))
-
 class TinyVGG_test(nn.Module):
     """
     TinyVGG architecture.
