5012b99c754153bf557adaf58b7efc52000d00cc
Xander Baatz <lorentzen.alexander@gmail.com>
2024-01-11 08:34:21 +0100

    Added vanilla TinyVGG model as "TinyVGG"

--- scripts/model.py
+++ scripts/model.py
@@ -6,7 +6,65 @@ PyTorch model code for TinyVGG model.
 import torch
 from torch import nn
 
-class TinyVGG2(nn.Module):
+# Vanilla TinyVGG model
+class TinyVGG(nn.Module):
+    """
+    TinyVGG architecture.
+    
+    Inspired by the TinyVGG architecture from the CNN explainer website.
+        See: https://poloclub.github.io/cnn-explainer/
+    
+    Args:
+        input_shape:  An integer indicating the number of input channels.
+        hidden_units: An integer indicating the number of hidden units between layers.
+        output_shape: An integer indicating the number of output units
+    """
+    def __init__(self,
+                 input_shape: int,
+                 hidden_units: int,
+                 output_shape: int) -> None:
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+          nn.Conv2d(in_channels=input_shape, 
+                    out_channels=hidden_units, 
+                    kernel_size=3,
+                    stride=1, 
+                    padding=0),  
+          nn.ReLU(),
+          nn.Conv2d(in_channels=hidden_units, 
+                    out_channels=hidden_units,
+                    kernel_size=3,
+                    stride=1,
+                    padding=0),
+          nn.ReLU(),
+          nn.MaxPool2d(kernel_size=2,
+                        stride=2)
+        )
+        self.conv_block_2 = nn.Sequential(
+          nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=0),
+          nn.ReLU(),
+          nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=0),
+          nn.ReLU(),
+          nn.MaxPool2d(2)
+        )
+        self.classifier = nn.Sequential(
+          nn.Flatten(),
+          # Where did this in_features shape come from? 
+          # It's because each layer of our network compresses and changes the shape of our inputs data.
+          nn.Linear(in_features=hidden_units*13*13,
+                    out_features=output_shape)
+        )
+    
+    def forward(self, x: torch.Tensor):
+        #x = self.conv_block_1(x)
+        #x = self.conv_block_2(x)
+        #x = self.classifier(x)
+        #return x
+        
+        # Using operator fusion (faster)
+        return self.classifier(self.conv_block_2(self.conv_block_1(x)))
+
+class TinyVGG_test(nn.Module):
     """
     TinyVGG architecture.
     
@@ -107,7 +165,7 @@ class TinyVGG2(nn.Module):
                                                           self.convolutional_block_1(x))
                )
 
-class TinyVGG(nn.Module):
+class TinyVGG_test_2(nn.Module):
     """
     TinyVGG architecture.
     
