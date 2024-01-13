ff569a47355c08312593205a48af38ef6efe4c83
Xander Baatz <lorentzen.alexander@gmail.com>
2024-01-13 10:31:05 +0100

    Engine save before modifications to enable learning rate scheduler.

--- scripts/engine.py
+++ scripts/engine.py
@@ -55,7 +55,7 @@ def train_step(model: torch.nn.Module,
 
         # 2. Calculate  and accumulate loss
         loss = loss_fn(y_pred, y)
-        train_loss += loss.item() 
+        train_loss += loss.item()
 
         # 3. Optimizer zero grad
         optimizer.zero_grad()
